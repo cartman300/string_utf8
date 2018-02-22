7f6b4e8df3ab52f7c23e38237e0cd10fa093f1c3
Saša Barišić <cartman300@net.hr>
2018-02-22 22:21:45 +0100

    Updated code

--- string_utf8/Program.cs
+++ string_utf8/Program.cs
@@ -14,15 +14,35 @@ namespace InteropUtils {
 		static void Main(string[] args) {
 			Test.Init();
 
-			StringUTF8 Str1 = "На берегу пустынных волн Стоял он, дум великих полн, И вдаль глядел.";
+			// Stores string as UTF8
+			StringUTF8 UTF8 = "На берегу пустынных волн Стоял он, дум великих полн, И вдаль глядел.";
 
+			// Stores string as ASCII
+			StringASCII ASCII = "Hello ASCII World!";
 
+			// Converts from ASCII to UTF8 because all the characters map
+			StringUTF8 UTF8_2 = ASCII;
 
-			using (StringUTF8.Disposable Str2 = Test.PrintString(Str1)) {
+			// Does not work
+			StringASCII ASCII_2 = UTF8_2;
 
+			// Get a "const char*" equivalent of supplied string
+			StringPtr Ptr = UTF8;
 
-				Console.WriteLine("Returned: {0}", Str2);
-			}
+			// Ditto
+			Ptr = ASCII;
+
+			// Does not convert anything, is equivalent of a static cast (works both ways)
+			StringASCII ASCII_3 = (StringPtr)UTF8;
+
+			// Call native P/Invoke method with an UTF8 and ASCII string
+			// All native functions should have the signature "MethodName(StringPtr String)"
+			Test.PrintString(UTF8);
+			Test.PrintString(ASCII);
+
+			// Call native P/Invoke method but explicitly encode compatible strings to UTF8
+			Test.PrintString((StringUTF8)"Hello UTF8 World!");
+			Test.PrintString((StringUTF8)ASCII);
 
 			Console.WriteLine("Done!");
 			Console.ReadLine();
@@ -34,6 +54,6 @@ namespace InteropUtils {
 		public static extern void Init();
 
 		[DllImport(nameof(Test))]
-		public static extern StringUTF8 PrintString(StringUTF8 Str);
+		public static extern StringPtr PrintString(StringPtr Str);
 	}
 }
--- string_utf8/string_utf8.cs
+++ string_utf8/string_utf8.cs
@@ -5,94 +5,132 @@ using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
-[StructLayout(LayoutKind.Explicit, Pack = 1)]
-public unsafe struct StringUTF8 {
-	[FieldOffset(0)]
-	IntPtr Bytes;
 
-	public class Disposable : IDisposable {
-		IntPtr DataBuffer;
-		int Length;
-		bool Disposed = false;
+[StructLayout(LayoutKind.Sequential, Pack = 1)]
+public unsafe struct StringPtr {
+	public IntPtr Pointer;
 
-		public Disposable(IntPtr DataBuffer, int Length) {
-			this.DataBuffer = DataBuffer;
-			this.Length = Length;
-		}
-
-		public void Dispose() {
-			if (Disposed)
-				return;
-			Disposed = true;
-
-			GCHandle SelfGCHandle = GCHandle.FromIntPtr(Marshal.ReadIntPtr(DataBuffer));
-			SelfGCHandle.Free();
-
-			Marshal.WriteIntPtr(DataBuffer, 0, IntPtr.Zero);
-			Marshal.WriteIntPtr(DataBuffer, IntPtr.Size, IntPtr.Zero);
-			Marshal.FreeHGlobal(DataBuffer);
-		}
-
-		public override string ToString() {
-			return this;
-		}
-
-		public static implicit operator string(Disposable Disp) {
-			return ReadFromIntPtr(Disp.DataBuffer + IntPtr.Size);
-		}
+	public int Strlen() {
+		int Len = 0;
+		while (Marshal.ReadByte(Pointer, Len) != 0)
+			Len++;
+		return Len;
 	}
+
+	public static implicit operator StringPtr(IntPtr Ptr) {
+		return new StringPtr() { Pointer = Ptr };
+	}
+
+	public static implicit operator IntPtr(StringPtr Ptr) {
+		return Ptr.Pointer;
+	}
+
+	public static implicit operator StringUTF8(StringPtr Ptr) {
+		return new StringUTF8(Ptr.Pointer, Ptr.Strlen());
+	}
+
+	public static implicit operator StringASCII(StringPtr Ptr) {
+		return new StringASCII(Ptr.Pointer, Ptr.Strlen());
+	}
+}
+
+public unsafe class StringUTF8 {
+	IntPtr Bytes;
+	public int Length { get; private set; }
 
 	public StringUTF8(string Str) {
-		// Create the UTF8 encoded string
-		byte[] UTF8Bytes = Encoding.UTF8.GetBytes(Str);
-
-		// Allocate unmanaged data buffer and copy string into it
-		int DataBufferSize = UTF8Bytes.Length + IntPtr.Size + 1;
-		IntPtr DataBuffer = Marshal.AllocHGlobal(DataBufferSize);
-		Bytes = DataBuffer + IntPtr.Size;
-		Marshal.Copy(UTF8Bytes, 0, Bytes, UTF8Bytes.Length);
-		Marshal.WriteByte(DataBuffer, DataBufferSize - 1, 0);
-
-		// Allocate the disposable which holds a reference to itself
-		Disposable Data = new Disposable(DataBuffer, DataBufferSize);
-		Marshal.WriteIntPtr(DataBuffer, 0, GCHandle.ToIntPtr(GCHandle.Alloc(Data)));
+		byte[] SafeBytes = Encoding.UTF8.GetBytes(Str);
+		Bytes = Marshal.AllocHGlobal(SafeBytes.Length);
+		Marshal.Copy(SafeBytes, 0, Bytes, SafeBytes.Length);
+		Length = SafeBytes.Length;
 	}
 
-	public static implicit operator Disposable(StringUTF8 Str) {
-		try {
-			IntPtr MetadataPtr = Marshal.ReadIntPtr(Str.Bytes - IntPtr.Size);
-			if (MetadataPtr == IntPtr.Zero)
-				return null;
+	public StringUTF8(IntPtr Bytes, int Length) {
+		this.Bytes = Marshal.AllocHGlobal(Length);
+		for (int i = 0; i < Length; i++)
+			Marshal.WriteByte(this.Bytes, i, Marshal.ReadByte(Bytes, i));
 
-			GCHandle H = GCHandle.FromIntPtr(MetadataPtr);
-			return H.Target as Disposable;
-		} catch {
-		}
+		this.Length = Length;
+	}
 
-		return null;
+	~StringUTF8() {
+		Marshal.FreeHGlobal(Bytes);
+	}
+
+	public override string ToString() {
+		byte[] SafeBytes = new byte[Length];
+		for (int i = 0; i < SafeBytes.Length; i++)
+			SafeBytes[i] = Marshal.ReadByte(Bytes, i);
+
+		return Encoding.UTF8.GetString(SafeBytes);
 	}
 
 	public static implicit operator StringUTF8(string Str) {
 		return new StringUTF8(Str);
 	}
 
-	public static implicit operator IntPtr(StringUTF8 Str) {
-		return Str.Bytes;
+	public static implicit operator string(StringUTF8 Str) {
+		return Str.ToString();
 	}
 
-	public static implicit operator string(StringUTF8 Str) {
-		return ReadFromIntPtr(Str.Bytes);
+	public static implicit operator StringPtr(StringUTF8 Str) {
+		return new StringPtr() { Pointer = Str.Bytes };
+	}
+
+	public static implicit operator IntPtr(StringUTF8 Str) {
+		return (StringPtr)Str;
+	}
+}
+
+
+public unsafe class StringASCII {
+	IntPtr Bytes;
+	public int Length { get; private set; }
+
+	public StringASCII(string Str) {
+		byte[] SafeBytes = Encoding.ASCII.GetBytes(Str);
+		Bytes = Marshal.AllocHGlobal(SafeBytes.Length);
+		Marshal.Copy(SafeBytes, 0, Bytes, SafeBytes.Length);
+		Length = SafeBytes.Length;
+	}
+
+	public StringASCII(IntPtr Bytes, int Length) {
+		this.Bytes = Marshal.AllocHGlobal(Length);
+		for (int i = 0; i < Length; i++)
+			Marshal.WriteByte(this.Bytes, i, Marshal.ReadByte(Bytes, i));
+
+		this.Length = Length;
+	}
+
+	~StringASCII() {
+		Marshal.FreeHGlobal(Bytes);
 	}
 
 	public override string ToString() {
-		return this;
+		byte[] SafeBytes = new byte[Length];
+		for (int i = 0; i < SafeBytes.Length; i++)
+			SafeBytes[i] = Marshal.ReadByte(Bytes, i);
+
+		return Encoding.ASCII.GetString(SafeBytes);
 	}
 
-	public static string ReadFromIntPtr(IntPtr Ptr) {
-		int Len = 0;
-		while (Marshal.ReadByte(Ptr, Len) != 0)
-			Len++;
+	public static implicit operator StringASCII(string Str) {
+		return new StringASCII(Str);
+	}
 
-		return Encoding.UTF8.GetString((byte*)Ptr, Len);
+	public static implicit operator string(StringASCII Str) {
+		return Str.ToString();
+	}
+
+	public static implicit operator StringPtr(StringASCII Str) {
+		return new StringPtr() { Pointer = Str.Bytes };
+	}
+
+	public static implicit operator IntPtr(StringASCII Str) {
+		return (StringPtr)Str;
+	}
+
+	public static implicit operator StringUTF8(StringASCII Str) {
+		return Str.ToString();
 	}
 }

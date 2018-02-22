3bdf31f005b717bc64936ad3f80b53509f41d4e7
Saša Barišić <cartman300@net.hr>
2018-02-22 17:48:02 +0100

    Added project files

---
+++ string_utf8/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InteropUtils {
+	class Program {
+		static void Main(string[] args) {
+
+			string_utf8 Str = "Hello World from UTF8! \"Š ć Ž đ Ł\"";
+			
+
+			Console.WriteLine("Done!");
+			Console.ReadLine();
+		}
+	}
+}
---
+++ string_utf8/string_utf8.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class string_utf8 {
+
+	public string_utf8(string Str) {
+
+	}
+
+	public static implicit operator string_utf8(string Str) {
+		return new string_utf8(Str);
+	}
+}
